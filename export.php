8a01692cd272361534843682cf083a7d12b8ea8e
juaniigauna <nachogauna83@gmail.com>
2021-02-01 11:35:28 -0300

    Bug fixed :)

--- export.php
+++ export.php
@@ -1,18 +1,16 @@
 <?php
 require_once "autoload.php";
 use Modules\DataBase;
-use Modules\Backup;
+use Modules\Backup\Export;
 
 if ($_POST) {
     $connection = new DataBase($_POST['host'], $_POST['user'], $_POST['password'], $_POST['db_name']);
     $collection = $connection->getCollection();
-    $backup = new Backup($collection);
-    $export = $backup->export();
-    $backup->import($export['path']);
-    // if ($export) {
-    //     $url = $export['path'];
-    //     echo "Backup completed: <a href='$url' download>Download</a> or <a href='view.php?json=$url' target='_blank'>View</a>";
-    // } else {
-    //     echo "Has been occurred an error while backup database.";
-    // }
+    $backup = new Export($collection);
+    $export = $backup->start();
+    if ($export) {
+        echo "Backup completed: <a href='$export' download>Download</a> or <a href='view.php?json=$export' target='_blank'>View</a>";
+    } else {
+        echo "Has been occurred an error while backup database.";
+    }
 }
